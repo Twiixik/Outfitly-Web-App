b9643ffb7dda52f4f1101a99ec586ea0a40553b3
Twiixik <119367167+Twiixik@users.noreply.github.com>
2024-12-18 11:33:38 +0100

    Fix routing basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
 
+// Set the basename for GitHub Pages
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/Outfitly-Web-App/"}>
+  <React.StrictMode>
+    <BrowserRouter basename="/Outfitly-Web-App/">
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
